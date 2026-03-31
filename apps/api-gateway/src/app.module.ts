ea0f7e213845a59628493c1e8affd7194d0c93cd
gauri <gauri@medianv.com>
2026-03-31 17:13:19 +0530

    monorepo done2

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Isse poore app mein env mil jayega
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -1,18 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { Product } from '@nykaa-monorepo/types';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   // private readonly PRODUCT_URL = 'http://localhost:3001/all-products';
   // private readonly CART_URL = 'http://localhost:3002/cart';
 
-  private readonly PRODUCT_URL = 'https://nykaa-product-service.onrender.com/all-products/all-products';
- private readonly CART_URL = 'https://nykaa-cart-service.onrender.com/cart';
+//   private readonly PRODUCT_URL = 'https://nykaa-product-service.onrender.com/all-products/all-products';
+//  private readonly CART_URL = 'https://nykaa-cart-service.onrender.com/cart';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+private readonly PRODUCT_BASE_URL = this.configService.get<string>('PRODUCT_SERVICE_URL');
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  private readonly CART_BASE_URL = this.configService.get<string>('CART_SERVICE_URL');
 
   async getProductsFromOtherService(): Promise<Record<string, Product[]>> {
-    const response = await axios.get(this.PRODUCT_URL);
+    const response = await axios.get(`${this.PRODUCT_BASE_URL}/all-products`);
 
     // console.log(
     //   `go to https://nykaa-product-service.onrender.com/all-products/all-products to see the products from Product Service`,
@@ -37,7 +46,7 @@ export class AppService {
   // Cart ki request 3002 par bhejo
   async addToCart(userId: string, product: any) {
     const response = await axios.post(
-      `${this.CART_URL}/add/${userId}`,
+      `${this.CART_BASE_URL}/cart/add/${userId}`,
       product,
     );
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
