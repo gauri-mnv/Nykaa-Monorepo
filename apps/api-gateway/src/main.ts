c6dd6fd959fa8e062e1045878d4fd33ace1ccca8
gauri <gauri@medianv.com>
2026-03-31 17:46:54 +0530

    monorepo done gatway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      '*',
-      'https://nykaa-frontend-pu8y.onrender.com',
-    ],
+    origin: ['*'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
